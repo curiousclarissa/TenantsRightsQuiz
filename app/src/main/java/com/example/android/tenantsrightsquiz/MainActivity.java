c4b4baf55b34f44dfc5c291db6f04fdf8513acc6
curiousclarissa <clarissa.jarem@gmail.com>
2018-07-17 14:39:40 -0700

    fixed bugs in score

---
+++ app/src/main/java/com/example/android/tenantsrightsquiz/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.android.tenantsrightsquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputFilter;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+    //starting score is zero
+    int score = 0;
+    /**
+     * This method is called when the submit button is clicked.
+     */
+    public String submit(View view) {
+        checkedScore();
+        EditText sdciField = findViewById(R.id.q3SDCI);
+        sdciField.setFilters(new InputFilter[]{new InputFilter.AllCaps()});
+        String sdciTranslate = sdciField.getText().toString();
+        EditText oheField = findViewById(R.id.q4OHE);
+        oheField.setFilters(new InputFilter[]{new InputFilter.AllCaps()});
+        String oheTranslate = oheField.getText().toString();
+        if (Objects.equals(sdciTranslate, getString(R.string.answer3))){
+            score += 1;
+        }
+        if (Objects.equals(oheTranslate, getString(R.string.answer4))){
+            score += 1;
+        }
+
+        Toast.makeText(this, "Your score is " + score, Toast.LENGTH_SHORT).show();
+        return String.valueOf(score);
+    }
+    private int checkedScore(){
+        //handle user checked boxes
+        CheckBox checkBoxState =  findViewById(R.id.checkbox_State);
+        CheckBox checkBoxFederal =  findViewById(R.id.checkbox_Federal);
+        CheckBox checkBoxCounty =  findViewById(R.id.checkbox_County);
+        CheckBox checkBoxCity =  findViewById(R.id.checkbox_City);
+
+        boolean stateProtections = checkBoxState.isChecked();
+        boolean federalProtections = checkBoxFederal.isChecked();
+        boolean cityProtections = checkBoxCity.isChecked();
+        boolean countyProtections = checkBoxCounty.isChecked();
+
+        if (cityProtections == true){
+            score += 1;
+        }
+        if (federalProtections == true){
+            score += 1;
+        }
+        if (stateProtections == true){
+            score += 1;
+        }
+        else if (countyProtections == true){
+            score = 0;
+        }
+        return score;
+    }
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_departmentalenforcementSDCI:
+                if (checked)
+                    score += 1;
+                break;
+
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
